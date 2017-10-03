8ac6820f7092c41f74c84d9048ffba7407f3311e
Dariell Vasquez <dariell@Dariells-MacBook-Air.local>
2017-10-03 19:36:21 -0400

    some psuedocode done

---
+++ assets/yelber.js
@@ -0,0 +1,38 @@
+// Global variables
+
+
+
+// user's radius from uber api
+// current location lat and long
+// price of ride 
+// food price
+// cuisine/ preference
+// driver's creditials embedded in app??
+// traffic information
+// array of restaurant choices
+// closed?
+// rating
+// reviews
+// address
+
+
+// login credentials 
+// modals
+
+
+// user will put in their food/price preference
+// we will retrieve their gps location from UBER API
+// they'll submit form
+// based on choices they choose, user will be shown a list (array of restaurant choices)
+// with variables of 
+// closed?
+// rating
+// price
+// radius from user
+// reviews
+// address
+
+
+// confirm uber or cancel uber
+
+// if user confirms, give options of different uberrides
